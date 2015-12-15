c480cd0169bbad3bcf5260dddd770e91774d8a72
44240 <44240@PC003434.coll.lr.local>
2015-12-15 13:21:35 +0000

    15/12/15 Buttons testing plan done

---
+++ number_c_p_gen.py
@@ -0,0 +1,8 @@
+for count in range(9):
+    print("2.00{0}".format(count))
+
+for count in range(10, 99):
+    print("2.0{0}".format(count))
+
+for count in range(100, 290):
+    print("2.{0}".format(count))
